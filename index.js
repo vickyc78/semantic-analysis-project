5bd47056d7130c8f0496da69aeefaa3875c1ae46
vickywohlig <vicky.chotaliya@wohlig.com>
2020-09-20 18:15:55 +0530

    current count add

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ let reviewRoutes = require("./controllers/ReviewController");
 // reviewRoutes(router);
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-//   );
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+  );
+  next();
+});
 // // A sample route
 app.get("/", (req, res) => res.send("Hello World!"));
 var temp = 0,
@@ -42,7 +42,7 @@ app.get("/:word", (req, res) => {
   console.log("temp temp", temp);
   let sendData = temp / count;
   console.log("KKKKKKKKK", sendData);
-  res.status(200).json(sendData);
+  res.status(200).json({ totalReview: sendData, currentReview: result.score });
 });
 app.use(cors());
 // // Start the Express server
